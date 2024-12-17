50399f3385035d928d051f3595afa0ac4a92420a
Park-MJeong <pmj7511@gmail.com>
2024-12-17 09:57:40 +0900

    test: domain 단위테스트

---
+++ src/test/java/com/restock/domain/product/ProductTest.java
@@ -0,0 +1,44 @@
+package com.restock.domain.product;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+    private Product product;
+    @BeforeEach
+    void setUp() {
+        product = new Product(1L, 1, 5);
+    }
+
+
+    @Test
+    @DisplayName("제품테이블 재고횟수,수량 증가테스트")
+    void add() {
+        Product newProduct = product.add();
+        assertEquals(product.getId(), newProduct.getId());
+        assertEquals(product.getRestockCount()+1, newProduct.getRestockCount());
+        assertEquals(product.getQuantity()+10, newProduct.getQuantity());
+    }
+
+    @Test
+    @DisplayName("제품테이블 수량감소테스트")
+    void decrease() {
+        product.decrease();
+        assertEquals(4, product.getQuantity());
+    }
+
+    @Test
+    @DisplayName("재고가0일때 반환값테스트")
+    void isSoldOut() {
+        assertFalse(product.isSoldOut());
+
+        // 재고를 0으로 만들어 품절 상태 테스트
+        for (int i = 0; i < 5; i++) {
+            product.decrease();
+        }
+        assertTrue(product.isSoldOut());
+    }
+}
---
+++ src/test/java/com/restock/domain/productNotificationHistory/ProductNotificationHistoryTest.java
@@ -0,0 +1,73 @@
+package com.restock.domain.productNotificationHistory;
+
+import com.restock.domain.product.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductNotificationHistoryTest {
+    private Product product;
+    private ProductNotificationHistory history;
+    private Long userId;
+    @BeforeEach
+    void setUp() {
+        product = new Product(1L, 1, 5);
+        userId = 5L;
+
+    }
+
+    @Test
+    @DisplayName("제품테이블에 재고가 들어왔을때")
+    void inProgress() {
+        history = ProductNotificationHistory.inProgress(product);
+        assertEquals(1L, history.getProductId());
+        assertEquals(1, history.getRestockCount());
+        assertEquals("IN_PROGRESS", history.getNotiStatus());
+        assertNull(history.getUserId());
+    }
+
+    @Test
+    @DisplayName("생성자테스트")
+    void testConstructor(){
+        history= new ProductNotificationHistory(product);
+        assertEquals(1L, history.getProductId());
+        assertEquals(1, history.getRestockCount());
+        assertEquals("IN_PROGRESS", history.getNotiStatus());
+    }
+    @Test
+    @DisplayName("제품이없을때")
+    void cancelBySoldOut() {
+        history = new ProductNotificationHistory(product);
+        history.cancelBySoldOut(userId);
+        assertEquals(1L, history.getProductId());
+        assertEquals(1, history.getRestockCount());
+        assertEquals("CANCELED_BY_SOLD_OUT", history.getNotiStatus());
+        assertEquals(5L,history.getUserId());
+    }
+
+    @Test
+    @DisplayName("에러발생")
+    void cancelByError() {
+        history = new ProductNotificationHistory(product);
+        history.cancelByError(userId);
+        assertEquals(1L, history.getProductId());
+        assertEquals(1, history.getRestockCount());
+        assertEquals("CANCELED_BY_ERROR", history.getNotiStatus());
+        assertEquals(5L,history.getUserId());
+    }
+
+    @Test
+    @DisplayName("모두완료")
+    void completed() {
+        history = new ProductNotificationHistory(product);
+        history.completed(userId);
+        assertEquals(1L, history.getProductId());
+        assertEquals(1, history.getRestockCount());
+        assertEquals("COMPLETED", history.getNotiStatus());
+        assertEquals(5L,history.getUserId());
+
+
+    }
+}
